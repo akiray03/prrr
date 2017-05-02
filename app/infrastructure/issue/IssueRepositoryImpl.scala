712cfd2e4937f5fd9fbc7e6d4f7dfc4a76314645
Akira Yumiyama <yumiyama.akira@gmail.com>
2017-05-02 22:49:54 +0900

    コメント追加

--- app/infrastructure/issue/IssueRepositoryImpl.scala
+++ app/infrastructure/issue/IssueRepositoryImpl.scala
@@ -7,6 +7,7 @@ import DefaultJsonProtocol._
 import library.json_protocol.ZonedDateTimeJsonProtocol._
 import skinny.http.{HTTP, Request, Response}
 import scala.collection.mutable
+import play.api.Logger
 
 import response.{IssueListResponse, IssueResponse}
 import response.IssueResponseProtocol._
@@ -14,9 +15,22 @@ import response.IssueResponseProtocol._
 import domains.issue.{IssueEntity, IssueRepository}
 import domains.issue_query.IssueQueryEntity
 
+/**
+  * GitHub Issue のレポジトリ実装
+  *
+  * GitHub API を呼び出し、IssueEntityを返す
+  *
+  * @param configuration
+  */
 class IssueRepositoryImpl @Inject()(
                                      configuration: play.api.Configuration
                                    ) extends IssueRepository{
+
+  /**
+    * GitHub API 認証用の HTTPヘッダを返す
+    *
+    * @return HTTP認証ヘッダ
+    */
   private def authorizationHeader(): String = {
     val githubToken = configuration.getString("secrets.github_token")
     if (githubToken.isEmpty) {
@@ -25,9 +39,17 @@ class IssueRepositoryImpl @Inject()(
     f"token ${githubToken.get}%s"
   }
 
+  /**
+    * IssueQueryの条件で GitHub API を呼び出し、その結果を IssueEntity に詰めて返す
+    *
+    * @param issueQuery Issue検索条件
+    * @return IssueEntityのシーケンス
+    */
   override def fetchIssues(issueQuery: IssueQueryEntity): List[IssueEntity] = {
     val req = queryBuilder(issueQuery).header("Authorization", authorizationHeader())
+    Logger.info(s"request to ${req.url}")
     val response: Response = HTTP.get(req)
+    // トップレベルが配列になっているJSONのParseに失敗するので苦肉の策...
     val body = """{"issues":""" + response.textBody + """}"""
 
     val jsonAst = JsonParser(body)
@@ -36,6 +58,12 @@ class IssueRepositoryImpl @Inject()(
     convertToIssueEntities(issues)
   }
 
+  /**
+    * IssueQueryの条件を GitHub API 呼び出しのクエリパラメータに変換する
+    *
+    * @param issueQuery　Issue検索条件
+    * @return HTTPリクエストオブジェクト
+    */
   private def queryBuilder(issueQuery: IssueQueryEntity): Request = {
     val url = f"https://api.github.com/repos/${issueQuery.ownerName}/${issueQuery.repositoryName}/issues"
     val req = Request(url)
@@ -77,10 +105,16 @@ class IssueRepositoryImpl @Inject()(
     req
   }
 
-  private def convertToIssueEntities(issues: IssueListResponse): List[IssueEntity] = {
-    val repoNames = fetchRepositoryNames(issues)
+  /**
+    * IssueResponse を IssueEntity に詰め直す
+    *
+    * @param issueList GitHub API から返されたIssue一覧
+    * @return IssueEntityのリスト
+    */
+  private def convertToIssueEntities(issueList: IssueListResponse): List[IssueEntity] = {
+    val repoNames = fetchRepositoryNames(issueList)
 
-    issues.issues.map(i =>
+    issueList.issues.map(i =>
       IssueEntity(
         number = i.number,
         fullName = repoNames.getOrElse(i.repository_url, ""),
@@ -93,10 +127,16 @@ class IssueRepositoryImpl @Inject()(
     )
   }
 
-  private def fetchRepositoryNames(issues: IssueListResponse): Map[String, String] = {
+  /**
+    * Issue に対応する Gitリポジトリの情報を取得し、 Map を返します
+    *
+    * @param issueList
+    * @return
+    */
+  private def fetchRepositoryNames(issueList: IssueListResponse): Map[String, String] = {
     val repoNames = mutable.HashMap.empty[String, String]
 
-    issues.issues.foreach(issue => {
+    issueList.issues.foreach(issue => {
       if (!repoNames.contains(issue.repository_url)) {
         val repoName = fetchRepositoryName(issue.repository_url)
         repoNames.put(issue.repository_url, repoName)
@@ -106,6 +146,14 @@ class IssueRepositoryImpl @Inject()(
     repoNames.toMap[String, String]
   }
 
+  /**
+    * Gitリポジトリの情報を取得し、 full_name を返します
+    *
+    *  https://github.com/akiray03/prrr の場合、 akiray03/prrr を返す
+    *
+    * @param repoURL Gitリポジトリの情報取得APIのURL
+    * @return Gitリポジトリの full_name
+    */
   private def fetchRepositoryName(repoURL: String): String = {
     val req = Request(repoURL).header("Authorization", authorizationHeader())
     val response: Response = HTTP.get(req)
--- app/infrastructure/issue/response/IssueResponse.scala
+++ app/infrastructure/issue/response/IssueResponse.scala
@@ -5,6 +5,27 @@ import DefaultJsonProtocol._
 import java.time.ZonedDateTime
 import library.json_protocol.ZonedDateTimeJsonProtocol._
 
+/**
+  * GitHub API v3 の返すユーザ情報
+  *
+  * @param login            GitHubのユーザ名
+  * @param id               GitHub内部のユーザID
+  * @param avatar_url       アバターURL
+  * @param gravatar_id      GravatarのID
+  * @param url              ユーザ情報取得APIのURL
+  * @param html_url         ユーザページのURL
+  * @param followers_url
+  * @param following_url
+  * @param gists_url
+  * @param starred_url
+  * @param subscriptions_url
+  * @param organizations_url
+  * @param repos_url
+  * @param events_url
+  * @param received_events_url
+  * @param `type`
+  * @param site_admin
+  */
 case class GitHubUser(
                       login: String,
                       id: Long,
@@ -24,6 +45,16 @@ case class GitHubUser(
                       `type`: String,
                       site_admin: Boolean
                     )
+
+/**
+  * GitHub API v3 の返すラベル情報
+  *
+  * @param id       GitHub内部のラベルID
+  * @param url      ラベル情報APIのURL
+  * @param name     ラベル名
+  * @param color    ラベル色
+  * @param default  デフォルトか否か
+  */
 case class IssueLabel(
                        id: Long,
                        url: String,
@@ -31,6 +62,23 @@ case class IssueLabel(
                        color: String,
                        default: Boolean
                      )
+
+/**
+  * GitHub API v3 の返すマイルストーン情報
+  *
+  * @param url            マイルストーンの情報取得APIのURL
+  * @param html_url       マイルストーンのURL
+  * @param id             マイルストーンのID
+  * @param number         マイルストーンの番号
+  * @param state          マイルストーンの状態
+  * @param title          マイルストーン名称
+  * @param open_issues    オープンされているIssueの数
+  * @param closed_issues  クローズされたIssueの数
+  * @param created_at     マイルストーンの作成日時
+  * @param updated_at     マイルストーンの更新日時
+  * @param closed_at      マイルストーンのクローズ日時
+  * @param due_on         マイルストーンの〆切日時
+  */
 case class IssueMilestone(
                            url: String,
                            html_url: String,
@@ -45,6 +93,15 @@ case class IssueMilestone(
                            closed_at: ZonedDateTime,
                            due_on: ZonedDateTime
                          )
+
+/**
+  * GitHub API v3 の返すプルリクエスト情報
+  *
+  * @param url        Pull Request情報APIのURL
+  * @param html_url   Pull Request の URL
+  * @param diff_url   diff URL
+  * @param patch_url  patch URL
+  */
 case class IssuePullRequest(
                              url: String,
                              html_url: String,
@@ -52,6 +109,32 @@ case class IssuePullRequest(
                              patch_url: String
                            )
 
+/**
+  * GitHub API v3 の返す Issue 詳細情報
+  *
+  * @param id
+  * @param url
+  * @param repository_url
+  * @param labels_url
+  * @param comments_url
+  * @param events_url
+  * @param html_url
+  * @param number
+  * @param state
+  * @param title
+  * @param body
+  * @param user
+  * @param labels
+  * @param assignee
+  * @param milestone
+  * @param locked
+  * @param comments
+  * @param pull_request
+  * @param closed_at
+  * @param created_at
+  * @param updated_at
+  * @param assignees
+  */
 case class IssueResponse(
                           id: Long,
                           url: String,
@@ -77,7 +160,13 @@ case class IssueResponse(
                           assignees: List[GitHubUser]
                         )
 
+/**
+  * GitHub API v3 の返す Issue 一覧
+  *
+  * @param issues
+  */
 case class IssueListResponse(issues: List[IssueResponse])
+
 
 object IssueResponseProtocol extends DefaultJsonProtocol {
   implicit val issueUserFormatter: JsonFormat[GitHubUser] = jsonFormat17(GitHubUser.apply)

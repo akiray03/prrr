b35daebeb270a81656cbbe993fb82b0e7d88577e
Akira Yumiyama <yumiyama.akira@gmail.com>
2017-05-05 10:10:10 +0900

    DBのidカラムに auto_increment を追加

--- conf/db/migration/default/V2__Create_issue_queries.sql
+++ conf/db/migration/default/V2__Create_issue_queries.sql
@@ -1,5 +1,5 @@
 create table issue_queries (
-  id int not null primary key,
+  id int not null primary key auto_increment,
   name varchar(255) not null,
   owner_name varchar(255) not null,
   repository_name varchar(255) not null,
